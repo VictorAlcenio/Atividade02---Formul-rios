271b38db1ee9a7cab68614698e7daf5edb900526
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-07-12 14:27:00 -0300

    desenvolvimento do algoritmo

---
+++ exercicio2_cor.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Seletor de Cores Interativo</title>
+</head>
+
+<body>
+    <h2>Seletor de Cores Interativo</h2>
+        <form method="POST" action="sel_cor.php">
+            R: <input type="text" name="red" size="3" /><br /><br />
+            G: <input type="text" name="green" size="3" /><br /><br />
+            B: <input type="text" name="blue" size="3" /><br /><br />
+            <input type = "submit" value = "Mostrar a Cor" />
+        </form>
+</body>
+</html>
