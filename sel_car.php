3b09e5803ee2125f077ed3ccb73d4da604ec87a8
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-07-12 14:24:52 -0300

    desenvolvimento do algoritmo

---
+++ sel_car.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Atividade Prática 02 -  Seletor de Carros</title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+     <h2>Sucesso!</h2>
+     <?php
+
+     $tipo = $_POST['selTipo'];
+     $cor = $_POST['txtCor'];
+
+     print"Seu $tipo de cor $cor está pronto. Dirija com cuidado!";
+     ?> 
+</body>
+</html>
